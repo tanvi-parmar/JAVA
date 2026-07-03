ac09a0ee43e3556bdf39935ba2fedfc771b7867e
tanvi-parmar <tanviparmar357@gmail.com>
2026-07-03 07:40:26 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,11 @@
+
+public class Address 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Shastri Nagar \"7/20\", ");
+        System.out.println("Near Ramapir Chowkdi,");
+        System.out.println("Gandhigram,Rajkot,");
+        System.out.println("Gujarat-360007");
+    }
+}
---
+++ Resume.java
@@ -0,0 +1,40 @@
+
+public class Resume 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("1.PERSONAL INFORMATION");
+        System.out.println("Parmar Tanvi Nareshbhai");
+        System.out.println("9408500356");
+        System.out.println("tanviparmar357@gmail.com");
+        System.out.println("Rajkot(Gujrat)");
+        System.out.println("\n");
+        
+        System.out.println(" 2.CAREER OBJECTIVE");
+        System.out.println("Motivated B.Sc.IT student seeking opportunities to apply programming and"
+                + " problem-solving skills while gaining practical experience in the IT industry.");
+        System.out.println("\n");
+        
+        System.out.println("3.EDUCATION");
+        System.out.println("B.Sc.IT,Atmiya University,Rajkot");
+        System.out.println("YOS : 2025-2028");
+        System.out.println("\n");
+        
+        System.out.println("4.TECHNICAL SKILLS");
+        System.out.println("Programming languages:C,Python,Java");
+        System.out.println("Web technologies:HTML,CSS,Javascript");
+        System.out.println("Tools:MS Office,VS COde");
+        System.out.println("\n");
+        
+        System.out.println("5.LANGUAGES KNOWN");
+        System.out.println("Gujarati");
+        System.out.println("English");
+        System.out.println("Hindi");
+        System.out.println("\n");
+        
+        System.out.println("6.HOBBIES&INTERESTS");
+        System.out.println("Coding");
+        System.out.println("Reading");
+        
+    }
+}
